f49f65f3dd715335f351d591d75ec93ee373671f
CarmenViolet <margaretholmes200@gmail.com>
2022-08-30 19:35:40 +0300

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMain = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+   return galleryItems.map(({preview, original, description}) => {
+        return  `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    }).join(' ');
+}
+
+
+galleryMain.insertAdjacentHTML('afterbegin', galleryMarkup);
+galleryMain.addEventListener('click', pickPictureGalleryItem);
+const biggerElPicture = document.querySelector(('img[data-source]'));
+
+
+function pickPictureGalleryItem(event) {
+    event.preventDefault();
+
+    const galleryLink = event.target.classList.contains('gallery__image');
+
+    if(!galleryLink) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" width="1000" height="600">
+    `)
+    instance.show();
+    
+    window.addEventListener('keydown', closeModalPicture);
+    
+    function closeModalPicture(event) {
+        if(event.code === 'Escape') {
+            instance.close()
+        }
+    };
+};
+
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMain = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+   return galleryItems.map(({preview, original, description}) => {
+        return  ` <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`
+    }).join(' ');
+}
+
+galleryMain.insertAdjacentHTML('afterbegin', galleryMarkup);
+galleryMain.addEventListener('click', pickPictureGalleryItem);
+
+function pickPictureGalleryItem(event) {
+    event.preventDefault()
+};
+
+var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250, captions: true});
